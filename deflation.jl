0e7fed37961a6c92d60b62564c5e932e3943ce94
Gianfranco Vieri <gv720@imperial.ac.uk>
2022-05-26 16:11:00 +0100

    g

--- deflation.jl
+++ deflation.jl
@@ -1,3 +1,59 @@
-function myfu()
+import Pkg
+
+Pkg.add("ForwardDiff")
+
+ 
+
+using ForwardDiff
+
+ 
+
+function M(x, x1, p=2, alpha=1)
+
+    1 / abs(x-x1)^p + alpha
 
 end
+
+ 
+
+function deflated_newton(x0, x1, f, max_iter=1000, epsilon=1e-10, p=2)
+
+    x = x0
+
+    i = 0
+
+    while abs(x-f(x)) > epsilon
+
+        x = x - 1 / ((-p * (x-x1) / abs(x-x1)^(p+2)) * f(x) + M(x, x1) * ForwardDiff.derivative(f, x)) * M(x, x1) * f(x)
+
+        if i > max_iter
+
+            return "Cannot converge."
+
+        end
+
+        i = i + 1
+
+    end
+
+    x
+
+end
+
+ 
+
+function test_deflated_newton(f, x0, x1)
+
+    deflated_newton(x0, x1, f)
+
+end
+
+ 
+
+f(x) = x^2 - 1
+
+ 
+
+deflated_newton(-0.1, 1, f)
+
+ 
