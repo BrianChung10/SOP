4f3c3bf3253fa0c08cfa0c777b39972531e846ef
fz920 <97394947+fz920@users.noreply.github.com>
2022-05-27 11:38:36 +0100

    deflated newton works in higher dimension

--- simple_deflation/deflation.jl
+++ simple_deflation/deflation.jl
@@ -4,7 +4,7 @@ Pkg.add("LinearAlgebra")
 using ForwardDiff
 using LinearAlgebra
 import ForwardDiff: derivative, jacobian
-import LinearAlgebra: norm
+import LinearAlgebra: norm, inv
 
 
 # Define the shifted deflation operator
@@ -29,9 +29,24 @@ function deflated_newton(x0, x1, f, max_iter=1000, epsilon=1e-13, p=2)
 end
 
 
+function deflated_newton_higher_dimension(x0, x1, f, max_iter=1000, epsilon=1e-13, p=2)
+    x = x0
+    i = 0
+    while norm(f(x)) > epsilon
+        temp_func(y) = M(y, x1) * f(y)
+        x = x - inv(jacobian(temp_func, x)) * M(x, x1) * f(x)
+        if i > max_iter
+            return "Cannot converge"
+        end
+        i = i + 1
+    end
+    x
+end
+
 f(x) = (x-1) * (x+1)
 g(x) = (x-2) * (x+2) * (x+3)
 h(x) = sin(x)
+f1(x) = (x .- [1, 1]) .* (x .- [2, 2])
 
 
 deflated_newton(-0.1, 1, f)
