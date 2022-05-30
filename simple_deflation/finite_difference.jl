dfedcda7608129f837f4b92374607b54efe0abc1
fz920 <97394947+fz920@users.noreply.github.com>
2022-05-30 16:55:25 +0100

    update finite difference

--- simple_deflation/finite_difference.jl
+++ simple_deflation/finite_difference.jl
@@ -1,4 +1,5 @@
-using ForwardDiff
+using ForwardDiff, LinearAlgebra
+import LinearAlgebra: inv
 import ForwardDiff: jacobian
 
 
@@ -12,13 +13,46 @@ lambda = 1
 # Implement the F[u] as defined in (3.7)
 function F(u::AbstractVector{T}) where T # F takes a vector of lengh n+1 and returns a vector of length n+1
     v = zeros(T, length(u))
-    for k = 1: n-2
+    v[2] = 1 / h^2 * (-2u[2] + u[3]) + lambda * exp(u[2])
+    for k = 2: n-2
         v[k+1] = 1 / h^2 * (u[k] - 2u[k+1] + u[k+2]) + lambda * exp(u[k+1])
     end
+    v[n] = 1 / h^2 * (u[n-1] - 2u[n]) + lambda * exp(u[n])
     v
 end
 
 y = zeros(n+1)
-jacobian(F, y)
+A = jacobian(F, y)
 
-F(y)
+# Make jacobian invertible while preserving the boundary conditions
+A[1, 1] = 1
+A[end, end] = 1
+
+function M(x, x1, p=2, alpha=1)
+    I(n+1) / norm(x-x1)^p + alpha * I(n+1)
+end
+
+
+function newton(f, x0, max_iter=5, eps=1e-13)
+    x = x0
+    i = 0
+    while norm(f(x)) > eps
+        if i > max_iter
+            return "Cannot converge"
+        end
+        A = jacobian(f, x)
+        # Have to make A invertible while preserving the boundary conditions
+        x = x - inv(A) * f(x)
+        i += 1
+    end
+    x
+end
+
+
+function deflated_newton(x0, x1, f)
+    g = x -> M(x, x1) * f(x)
+    newton(g, x0)
+end
+
+x0 = zeros(n+1)
+x1 = newton(F, x0)
