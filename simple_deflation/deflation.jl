a266a4b6ce3473601ccc8bda4db74766323bfbfa
Brian Chung <brian.chung20@imperial.ac.uk>
2022-05-30 17:13:27 +0100

    e

--- simple_deflation/deflation.jl
+++ simple_deflation/deflation.jl
@@ -59,7 +59,7 @@ end
 function deflated_newton_solve_1d(f, x0)
     x = newton(f, x0)
     solution = [x]
-    while x != "Cannot converge"
+    while typeof(x) == Number
         f = y -> f(y) * M(y, x)
         x0 = rand(-1000: 1000)
         x = newton(f, x0)
@@ -76,26 +76,51 @@ i(x) = last(fs)(x) * (x-1)
 i(5)
 
 
-function multroot(f, x0)
-    fs = Vector{Function}(undef, 1)
+function multroot(f, x0, n = 10)
+    fs = Vector{Function}(undef, n)
     fs[1] = f
-    x1 = newton(f, x0)
-    sol = [x1]
-    xn = x1
-    while xn != "Cannot converge"
+    xn = newton(f, 0)
+    sol = [xn]
+    k = 1
+    while xn != "Cannot converge."
+        k += 1
         xn = last(sol)
-        g(x) = last(fs)(x) * M(x, xn)
-        push!(fs, g)
-        xnew = newton(g, x0)
+        fs[k] = x -> fs[k-1](x) * M(x, xn)
+        xnew = newton(fs[k], x0)
         push!(sol, xnew)
     end
     sol
 end
 
+multroot(g, 0)
+
+
+
+fs = Vector{Function}(undef, 10)
+fs[1] = g
+sol = [newton(g,0)]
+newton(g,0)
+fs[2] = x -> fs[1](x) * M(x, -3)
+newton(fs[2], 0)
+fs[3] = x -> fs[2](x) * M(x, -2)
+newton(fs[3], 0)
+fs[4] = x -> fs[3](x) * M(x, 2)
+newton(fs[4], 0)
+
+
+sol = [x1]
+xn = last(sol)
+fun(x) = last(fs)(x) * M(x, xn)
+newton(fun, 0)
+fun1(x) = fun(x) * M(x, -2)
+newton(fun1, 0)
+fun2(x) = fun1(x) * M(x, 2)
+newton(fun2, 0)
+
 
 
 # Test functions
-f(x) = (x-1) * (x+1)
+f(x) = (x-1) * (x-2)
 g(x) = (x-2) * (x+2) * (x+3)
 h(x) = sin(x)
 f1(x) = (x .- [1, 1]) .* (x .- [2, 2])
