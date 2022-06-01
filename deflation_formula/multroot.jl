8a42cf65010c878a2d7dc5bd7a1036c36c0d7920
Sissiy2018 <fuming.yang20@imperial.ac.uk>
2022-06-01 21:01:53 +0800

    error in solve_nd -- fake roots

--- deflation_formula/multroot.jl
+++ deflation_formula/multroot.jl
@@ -32,7 +32,7 @@ function deflated_newton(x0, x1, f, max_iter=1000, epsilon=1e-13, p=2)
 end
 
 # Implement the deflated_newton in higher dimension
-function deflated_newton_higher_dimension(x0, x1, f, max_iter=100, epsilon=1e-13, p=2)
+function deflated_newton_higher_dimension(x0, x1, f, max_iter=1000, epsilon=1e-13, p=2)
     x = x0
     i = 0
     while norm(f(x)) > epsilon
@@ -50,17 +50,37 @@ function deflated_newton_higher_dimension(x0, x1, f, max_iter=100, epsilon=1e-13
     x
 end
 
+function M1(x, sol, p=2, alpha=1)
+    m=1
+    for sols in sol
+        m = m * (1 / norm(x-sols)^p + alpha)
+    end
+    return m
+end
+
 # Solve for all roots by using deflated newton method
-function deflated_newton_solve_1d(x0, x1, f,max_iter= 1000)
+function deflated_newton_solve_1d(x0, x1, f)
     x = deflated_newton(x0, x1, f)
     solution = [x]
-    fs = Array{Function}(undef,max_iter)
-    fs[1] = f
-    fs[2] = x -> fs[1](x)*M(x,x1)
-    k = 2
-    for k = 2:max_iter-1
-        x = deflated_newton(x0, x, fs[k])
-        fs[k+1] = y -> fs[k](y)*M(y,x)
+    fs = x -> f(x)*M(x,x1)
+    while true
+        x = deflated_newton(x0, x, fs)
+        fs = y -> f(y) * M1(y,solution)
+        if x == "Cannot converge."
+            return solution
+        else
+            push!(solution, x)
+        end
+    end
+end
+
+function deflated_newton_solve_nd(x0, x1, f)
+    x = deflated_newton_higher_dimension(x0, x1, f)
+    solution = [x]
+    fs = x -> f(x)*M(x,x1)
+    while true
+        x = deflated_newton_higher_dimension(x0, x, fs)
+        fs = y -> f(y) * M1(y,solution)
         if x == "Cannot converge."
             return solution
         else
@@ -74,6 +94,7 @@ f(x) = (x-1) * (x+1)
 g(x) = (x-2) * (x+2) * (x+3)
 h(x) = sin(x)
 f1(x) = (x .- [1, 1]) .* (x .- [2, 2])
+f2(x) = (x .- [1, 1]) .* (x .- [2, 2]).*(x .- [3, 3])
 z(x) = (x-1) * (x-2) * (x-3) * (x-4)
 
 # Tests
@@ -83,3 +104,5 @@ deflated_newton(0.1, 0, h)
 deflated_newton_higher_dimension([0.1,0.1],[1, 1],f1)
 deflated_newton_solve_1d(1,2,g)
 deflated_newton_solve_1d(0,1,z)
+deflated_newton_solve_nd([0.1,0.1],[1, 1],f2)
+deflated_newton_solve_nd([0.1,0.1],[1, 1],f1)
