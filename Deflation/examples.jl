3ddc1bfc1177022678f160ddf3ab546805c65fee
fz920 <97394947+fz920@users.noreply.github.com>
2022-06-05 21:25:31 +0100

    update

--- Deflation/examples.jl
+++ Deflation/examples.jl
@@ -1,12 +1,14 @@
 import Pkg
-Pkg.add("Plots")
+Pkg.add("PlotlyJS")
+Pkg.add("LaTeXStrings")
 using Plots
-using ForwardDiff, LinearAlgebra
+using ForwardDiff, LinearAlgebra, LaTeXStrings
 import ForwardDiff: derivative, jacobian
 import LinearAlgebra: norm, inv, I
 
+
 function M(x, x1, p=2, alpha=1)
-    I(2) / norm(x-x1)^p + alpha * I(2)
+    1 / norm(x-x1)^p + alpha
 end
 
 
@@ -37,11 +39,11 @@ g(x, y) = x^2 - y
 f1(x) = 4x^3 - 3x
 g1(x) = x^2
 
-plot(f1, -1.5, 1.5)
-plot!(g1, -1.5, 1.5)
+plot(f1, -1.5, 1.5, label=L"5x^2-4x-y=0", title="The Cubic-Parabola Example")
+plot!(g1, -1.5, 1.5, label=L"3x^2+x-y=0")
 
 
-x0 = [0.5; 0.5] # Initial guess
+x0 = [0.4; 0.9] # Initial guess
 h1(x) = [f(x[1], x[2]); g(x[1], x[2])] # Original function (Cubic-Parabola)
 x1 = newton(h1, x0) # Find the first root
 h2(x) = M(x, x1) * h1(x) # Define the deflated function
@@ -50,30 +52,49 @@ h3(x) = M(x, x2) * h2(x) # Define the second deflated function
 x3 = newton(h3, x0)
 sol = [x1, x2, x3]
 
-struct DeflatedFunction{F}
-    x::Vector{Float64} # points
-    f::F # original function
-end
-
-function (D::DeflatedFunction)(x)
-    ret = D.f(x)
-    for x in D.x
-        # TODO: update ret
+# Define a function to test whether r = (0, 0) is a magnetic zero
+function mag_zero()
+    success = 0
+    fail = 0
+    for i = 0: 0.1: 1
+        for j = 0: 0.1: 1
+            x0 = [i; j]
+            x1 = newton(h1, x0)
+            if norm(x1 - [0; 0]) < 1e-9
+                success += 1
+            else
+                fail += 1
+            end
+        end
     end
-    ret
+    println(success)
+    println(fail)
 end
 
+mag_zero()
 
 
-# Example 2: The Four-Cluster
-f(x, y) = (x - y^2) * (x - sin(y))
-g(x, y) = (cos(y) - x) * (y - cos(x))
 
-
-function four_cluster(x)
-    [f(x[1], x[2]); g(x[1], x[2])]
+# The 3x3 system
+function f(x)
+    return sin(x[1]) + cos(x[2])
 end
 
+n = 100
+x = range(-1, 1, n)
+y = range(-1,1,n)
 
-x0 = [0.9; 1]
-newton(four_cluster, x0)
+xgrid = repmat(x',n,1)
+ygrid = repmat(y,1,n)
+
+z = zeros(n,n)
+
+for i in 1:n
+    for j in 1:n
+        z[i:i,j:j] = f([x[i],y[j]])
+    end
+end
+
+plot_wireframe(xgrid,ygrid,z)
+
+## new line
