0ec159d9e4505619eb381f8be4aa9c776895584a
fz920 <97394947+fz920@users.noreply.github.com>
2022-06-09 00:12:00 +0100

    update goal 11

--- Finite_Differences_2D/Goal11.jl
+++ Finite_Differences_2D/Goal11.jl
@@ -12,16 +12,17 @@ end
 n = 100
 A = fd_2d(n)
 
+
 n = 100
-μ = 0.2
+μ = 0.4
 function F(u::AbstractVector{T}) where T # F takes a vector of lengh (n-1)^2 and returns a vector of length (n-1)^2
     ω = 0.2 # ω is fixed at 0.2
-    A = A = fd_2d(n)
+    A = fd_2d(n)
     xrange = range(-12, 12, length=n+1)
     yrange = range(-12, 12, length=n+1)
     h = step(xrange)
 
-    v = -1/2 * (1/h) * A * u # First termS
+    v = 1/2 * (1 / h^2) * A * u # First term
     v += abs.(u) .^ 2 .* u # Second term
     index = 1
     for i = 2: length(xrange)-1  # Third term
@@ -35,7 +36,7 @@ function F(u::AbstractVector{T}) where T # F takes a vector of lengh (n-1)^2 and
 end
 
 
-function M(x, x1, p=2, alpha=1)
+function M(x, x1, p=4, alpha=1) # Modified deflation operator
     1 / norm(x-x1)^p + alpha
 end
 
@@ -62,6 +63,5 @@ end
 
 # We obtain the solution of the ODE via (2.6)
 x0 = zeros((n-1)^2)
-x1 = newton(F, x0)
-x0[2] = 1
+x1 = newton(F, x0) 
 x2 = deflated_newton(x0, x1, F) # Two solutions for the ODE
