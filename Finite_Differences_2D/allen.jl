22e508f67c4fb8210a8c929187f35dc65484b267
fz920 <97394947+fz920@users.noreply.github.com>
2022-06-14 17:45:55 +0100

    update allen equation

--- Finite_Differences_2D/allen.jl
+++ Finite_Differences_2D/allen.jl
@@ -3,36 +3,26 @@ import ForwardDiff: derivative, jacobian, gradient
 import LinearAlgebra: norm, inv
 import SparseArrays: sparse
 
-n = 100
-function make_U(n) # Function that produces the matrix satisfies the boundary condition
-    xrange = range(0, 1, length=n+1)
-    yrange = range(0, 1, length=n+1)
-    h = step(xrange)
-
-    A = zeros(n+1, n+1)
-    # Need to satisfy the boundary condition
-    for i = 1: n+1
-        for j = 1:n+1
-            if (j == 1 || j == n+1) && (i != 1 && i != n+1)
-                A[i, j] = 1
-            elseif i == 1 || i == n+1
-                A[i, j] = -1
-            end
-        end
-    end
-A
-end
-
-A = make_U(10)
-
-
+n = 50
 
 δ = 0.04
+
 # Define function F in this case
 function F(u::AbstractVector{T}) where T
     # F takes a vector of length (n+1)^2 and returns a vector of (n+1)^2
     U = reshape(u, (n+1, n+1))'
-    v = make_U(n)
+    v = zeros(T, n+1, n+1)
+
+    # Need to satisfy the boundary condition
+    for i = 1: n+1
+        for j = 1:n+1
+            if (j == 1 || j == n+1) && (i != 1 && i != n+1)
+                v[i, j] = 1
+            elseif i == 1 || i == n+1
+                v[i, j] = -1
+            end
+        end
+    end
 
     xrange = range(0, 1, length=n+1)
     yrange = range(0, 1, length=n+1)
@@ -43,8 +33,7 @@ function F(u::AbstractVector{T}) where T
             v[i, j] = -δ * (4U[i, j] + U[i, j-1] + U[i, j+1] + U[i-1, j] + U[i+1, j]) - h^2 * 1/δ * (U[i, j]^3 - U[i, j])
         end
     end
-
-    convert(Vector{Float64}, vec(v'))
+    v = vec(v')
 end
 
 
@@ -62,5 +51,35 @@ function newton(f, x0, max_iter=1000, eps=1e-8)
     x
 end
 
+U = make_U(n)
+U[2:end-1, 2:end-1] .= 1
+x0 = vec(U') # Initial guess that satisfies the boundary condition
 
 x1 = newton(F, x0)
+
+A = jacobian(F, x0)
+sum(A)
+
+F(x0)[100: 200]
+
+
+
+# This function helps us to produce the initial guess
+function make_U(n)
+    xrange = range(0, 1, length=n+1)
+    yrange = range(0, 1, length=n+1)
+    h = step(xrange)
+
+    A = zeros(n+1, n+1)
+    # Need to satisfy the boundary condition
+    for i = 1: n+1
+        for j = 1:n+1
+            if (j == 1 || j == n+1) && (i != 1 && i != n+1)
+                A[i, j] = 1
+            elseif i == 1 || i == n+1
+                A[i, j] = -1
+            end
+        end
+    end
+A
+end
